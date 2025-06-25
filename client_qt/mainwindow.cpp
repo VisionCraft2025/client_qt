e60eed6457bba21a5a23eae30b149e707d6786b1
MIN <whitedaisy12759@gmail.com>
2025-06-25 15:04:11 +0900

    Feat/#2: Display RTSP video stream from Raspberry Pi in Qt

--- client_qt/mainwindow.cpp
+++ client_qt/mainwindow.cpp
@@ -1,14 +1,30 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // 라파 카메라 스트리머 객체 생성 (URL은 네트워크에 맞게 수정해야 됨
+    rpiStreamer = new Streamer("rtsp://192.168.0.76:8554/test", this);
+
+    // signal-slot
+    connect(rpiStreamer, &Streamer::newFrame, this, &MainWindow::updateRPiImage);
+    rpiStreamer->start();
 }
 
 MainWindow::~MainWindow()
 {
+    rpiStreamer->stop();
+    rpiStreamer->wait();
     delete ui;
+}
+
+void MainWindow::updateRPiImage(const QImage& image)
+{
+    // 영상 QLabel에 출력
+    ui->labelCamRPi->setPixmap(QPixmap::fromImage(image).scaled(
+        ui->labelCamRPi->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
--- client_qt/mainwindow.h
+++ client_qt/mainwindow.h
@@ -2,11 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QImage>
+#include "streamer.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +17,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void updateRPiImage(const QImage& image); // 라파캠 영상 표시
+
 private:
     Ui::MainWindow *ui;
+    Streamer* rpiStreamer;
 };
+
 #endif // MAINWINDOW_H
