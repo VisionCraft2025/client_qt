167a3d40660b9684065135ad3d490ed47e05a723
MIN <whitedaisy12759@gmail.com>
2025-07-28 03:31:45 +0900

    Style/#68: Apply global font style

--- client_qt/main.cpp
+++ client_qt/main.cpp
@@ -1,13 +1,39 @@
 #include "mainwindow.h"
 #include "home.h"
 #include <QApplication>
+#include <QFontDatabase>
+#include <QFont>
+#include <QDebug>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    //MainWindow w;
-    //w.show();
+    QString fontPath = ":/fonts/fonts/05HanwhaGothicR.ttf";
+    int fontId = QFontDatabase::addApplicationFont(fontPath);
+
+    if (fontId == -1) {
+        qDebug() << "font error!";
+        qDebug() << "    - path:" << fontPath;
+        qDebug() << "    - file:" << QFile::exists(fontPath);
+    } else {
+        QStringList families = QFontDatabase::applicationFontFamilies(fontId);
+        qDebug() << "font ID:" << fontId;
+        qDebug() << "    - familylist:" << families;
+
+        if (!families.isEmpty()) {
+            QFont defaultFont;
+            defaultFont.setFamily(families.at(0));
+            defaultFont.setPointSize(10);
+            defaultFont.setStyleStrategy(QFont::PreferAntialias);
+            a.setFont(defaultFont);
+            qDebug() << "[✓] nomal font:" << families.at(0);
+        } else {
+            qWarning() << "font family empty";
+        }
+    }
+
     Home home;
     home.show();
     return a.exec();
